a94b28a52168b11dfd0f730ac9d51be083e8418a
hammer1812 <philip-foerster@t-online.de>
2016-04-20 02:00:01 +0200

    changed ArrayList<Double> polynom to double[] polynom

--- src/Polynome/Polynom.java
+++ src/Polynome/Polynom.java
@@ -4,47 +4,48 @@ import java.util.ArrayList;
 
 public class Polynom {
 
-	private ArrayList<Double> polynom;
+	private double[] polynom;
 	
 	public Polynom(String argumente){
 		String[] coeffs = argumente.split(",");
-		polynom = new ArrayList<Double>();
+		polynom = new double[coeffs.length];
 		for(int i=0; i<coeffs.length; i++){
-			polynom.add(Double.parseDouble(coeffs[i]));
+			polynom[i] = Double.parseDouble(coeffs[i]);
 		}
 	}
 	
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<polynom.size();i++){
+		for(int i=0; i<polynom.length;i++){
 			//s = s + polynom[i] + "*x^" + i + "+";
+			
 			if(i==0){
-				sb.append(polynom.get(i));
+				sb.append(polynom[i]);
 				sb.append("*x^");
-				sb.append(i);
+				sb.append(polynom.length-i-1);
 				sb.append(" ");
 			}	//a*x^i
-			else if(i==polynom.size()-1){
-				if(polynom.get(i) >= 0){
+			else if(i==polynom.length-1){
+				if(polynom[i] >= 0){
 					sb.append("+");
-					sb.append(polynom.get(i));
+					sb.append(polynom[i]);
 				}	//+a
 				else{
-					sb.append(polynom.get(i));
+					sb.append(polynom[i]);
 				}	//-a
 			}
 			else{
-				if(polynom.get(i) >= 0){
+				if(polynom[i] >= 0){
 					sb.append("+");
-					sb.append(polynom.get(i));
+					sb.append(polynom[i]);
 					sb.append("*x^");
-					sb.append(i);
+					sb.append(polynom.length-i-1);
 					sb.append(" ");
 				}	//+a*x^i
 				else{
-					sb.append(polynom.get(i));
+					sb.append(polynom[i]);
 					sb.append("*x^");
-					sb.append(i);
+					sb.append(polynom.length-i-1);
 					sb.append(" ");
 				}	//-a*x^i
 			}
--- src/Polynome/PolynomTabulator.java
+++ src/Polynome/PolynomTabulator.java
@@ -4,7 +4,7 @@ public class PolynomTabulator {
 
 	public static void main(String[] args) {
 
-		Polynom poly = new Polynom("5,-3,8.8");
+		Polynom poly = new Polynom("-1,-1,5.6,700,1,2");
 		System.out.println(poly.toString());
 		
 	}
