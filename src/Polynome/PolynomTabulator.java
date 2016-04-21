74a8800c3ab07c5230777656c6701a29ad2480f3
hammer1812 <philip-foerster@t-online.de>
2016-04-21 14:12:05 +0200

    finished PolynomTabulator
    stellt alles korrekt dar, es fehlt nur ne ordentliche Formatierung
    FeelsBadMan

--- src/Polynome/PolynomTabulator.java
+++ src/Polynome/PolynomTabulator.java
@@ -1,21 +1,45 @@
 package Polynome;
 
+import java.util.ArrayList;
+
 public class PolynomTabulator {
+	
+	private ArrayList<String> table;
+	private Horner hn;
 
 	public PolynomTabulator(Horner hn){
+		this.hn = hn;
 		//funktionswerte für wertebereich berechnen
+		table = new ArrayList<String>();
+		
 		for(double i = hn.getWertebereich1(); i<=hn.getWertebereich2(); i += hn.getSchrittweite()){
-			
-			
-			
+			StringBuilder sb = new StringBuilder();
+			sb.append("\t");
+			sb.append(i);
+			sb.append("\t\t");
+			sb.append(hn.getFunktionswert(i));
+			sb.append("\t\t");
+			sb.append(hn.getErsteAbleitungwert(i));
+			table.add(sb.toString());
 		}
 		
 	}
-	
-	public String format(){
-		String s = "";
-		
-		return s;
+	//Muster:
+	//    5x^2+3x+1
+	//_______________________________________________________________________
+	//|		Argument	|		Polynomwert		|		Ableitungswert		|
+	//|		x			|		xyz				|		XYZ					|
+	//|		x			|		xyz				|		XYZ					|
+	//|		x			|		xyz				|		XYZ					|
+	//|		x			|		xyz				|		XYZ					|
+	//|		x			|		xyz				|		XYZ					|
+	//|_________________|_______________________|___________________________|
+	public void format(){
+		System.out.println("Berechnung für das Polynom: " + hn.getStringOfPolynom());
+		System.out.println("\tArgument\t\tPolynomwert\t\tWert der ersten Ableitung");
+		for(int i = 0; i<table.size(); i++){
+			System.out.println(table.get(i));
+		}
 	}
 	
 }
