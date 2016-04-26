c940629a4a4e1769532b84bf3158be5928a59c61
hammer1812 <philip-foerster@t-online.de>
2016-04-26 02:17:39 +0200

    Ausgabe formatiert
    Ich habe noch nie so eine schöne Tabelle gesehen
    PolynomTabulator > Excel

--- src/Polynome/PolynomTabulator.java
+++ src/Polynome/PolynomTabulator.java
@@ -15,26 +15,34 @@ public class PolynomTabulator {
 		double ende = Math.abs(wertebereich1-wertebereich2)/schrittweite;
 		
 		for(int n = 0; n <= ende; n++){
+			StringBuilder sb = new StringBuilder();
 			BigDecimal bd = new BigDecimal(wertebereich1 + schrittweite*n);
 			bd = bd.setScale(1, RoundingMode.HALF_UP);
-			StringBuilder sb = new StringBuilder();
+			//sb.append(bd.doubleValue());
+			//sb.append("\t\t");
+			sb.append(String.format("%1$-4f", bd.doubleValue()));
+			//sb.append(hn.getFunktionswert(bd.doubleValue()));
 			sb.append("\t");
-			sb.append(bd.doubleValue());
-			sb.append("\t\t");
-			sb.append(hn.getFunktionswert(bd.doubleValue()));
-			sb.append("\t\t");
-			sb.append(hn.getErsteAbleitungwert(bd.doubleValue()));
+			sb.append(String.format("%1$-7f", hn.getFunktionswert(bd.doubleValue())));
+			sb.append("\t");
+			//sb.append(hn.getErsteAbleitungwert(bd.doubleValue()));
+			sb.append(String.format("%1$-7f", hn.getErsteAbleitungwert(bd.doubleValue())));
+			sb.append("\n");
 			table.add(sb.toString());
 		}
 		
 	}
 
 	public void format(){
-		System.out.println("Berechnung für das Polynom: " + hn.getStringOfPolynom());
-		System.out.println("\tArgument\t\tPolynomwert\t\tWert der ersten Ableitung");
+		StringBuilder sb = new StringBuilder();
+		System.out.println("Berechnung für das Polynom: " + hn.getStringOfPolynom() + "\n");
+		System.out.println("Argument\tPolynomwert\tWert der ersten Ableitung");
 		for(int i = 0; i<table.size(); i++){
-			System.out.println(table.get(i));
+			//System.out.println(table.get(i));
+			sb.append(table.get(i));
+			
 		}
+		System.out.println(sb.toString());
 	}
 	
 }
